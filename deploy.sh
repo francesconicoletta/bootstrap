01f41e80249e7b1a4739b9bea645838555dab75f
Francesco Nicoletta Puzzillo <francesconico86@gmail.com>
2021-04-27 12:18:56 +0200

    fix: zsh configuration

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,8 @@ printf "\n"
 echo $PASSWORD | sudo -S apt update
 echo $PASSWORD | sudo -S apt install ansible git
 
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd $dir
+
 ansible-playbook -K dotfiles.yml
 
